39bb46848d9d355dea115e0c4da0ce8c99ec411b
Pavel Averin <st107211@student.spbu.ru>
2023-02-22 18:14:40 +0300

    Fix namespaces, replace "int[]" with "var", make all the methods start with a capital letter, change "public static" to "static public", changed "net6.0" to "net7.0".

--- Homework1/Sort/main.cs
+++ Homework1/Sort/main.cs
@@ -1,116 +1,42 @@
-﻿using System;
+﻿namespace QuickSort;
 
-namespace quickSort
+using System;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (Sort.Tests())
         {
-            if (Sort.Tests())
+            Console.Write("Eneter a length of the array: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            var array = new int[length];
+            for (int i = 0; i < length; i++) 
             {
-                Console.Write("Eneter a length of the array: ");
-                int length = Convert.ToInt32(Console.ReadLine());
-                int[] array = new int[length];
-                for (int i = 0; i < length; i++) 
-                {
-                    Console.Write($"Enter the element number {i}: ");
-                    array[i] = Convert.ToInt32(Console.ReadLine());
-                }
-                Console.Write("Array before sort: ");
-                DisplayArray(array);
-                Sort.quickSort(array);
-                Console.Write("Array after sort: ");
-                DisplayArray(array);
+                Console.Write($"Enter the element number {i}: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
             }
-            else
-            {
-                Console.WriteLine("Tests weren't passed");
-            }
+            Console.Write("Array before sort: ");
+            DisplayArray(array);
+            Sort.QuickSort(array);
+            Console.Write("Array after sort: ");
+            DisplayArray(array);
         }
-        static void DisplayArray(int[] array)
+        else
         {
-            foreach (int item in array)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+            Console.WriteLine("Tests weren't passed");
         }
+    }
+    static void DisplayArray(int[] array)
+    {
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
         
-    }
-    static class Sort
-    {
-        static public void quickSort(int[] array)
-        {
-            quickSortRecursion(array, 0, array.Length - 1);
-        }
-        static void quickSortRecursion(int[] array, int low, int high)
-        {
-            if (low < high)
-            {
-                int pivotIndex = partition(array, low, high);
-                quickSortRecursion(array, low, pivotIndex - 1);
-                quickSortRecursion(array, pivotIndex + 1, high);
-            } 
-        }
-        static int partition(int[] array, int low, int high)
-        {
-            int pivotValue = array[high];
-            int i = low;
-            for (int j = low; j < high; j++) 
-            {
-                if (array[j] <= pivotValue) 
-                {
-                    Swap(ref array[j], ref array[j]);
-                    i++;
-                }   
-            }
-            Swap(ref array[i], ref array[high]);
-            return i;
-        }
-        static void Swap(ref int oneValue, ref int anotherValue)
-        {
-            int temp = oneValue;
-            oneValue = anotherValue;
-            anotherValue = temp;
-        }
-        static bool IsSorted(int[] array)
-        {
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        static public bool Tests()
-        {
-            int[] testArray0 = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-            Sort.quickSort(testArray0);
-            if (!IsSorted(testArray0))
-            {
-                Console.WriteLine("Tests failed on a reversed array");
-                return false;
-            }
-
-            int[] testArray2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            Sort.quickSort(testArray2);
-            if (!IsSorted(testArray2))
-            {
-                Console.WriteLine("Tests failed on a sorted array");
-                return false;
-            }
-
-            int[] testArray3 = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
-            Sort.quickSort(testArray3);
-            if (!IsSorted(testArray3))
-            {
-                Console.WriteLine("Tests failed on an array of the same element");
-                return false;
-            }
-            
-            return true;
-        }
-    }
 }
+
+
+
