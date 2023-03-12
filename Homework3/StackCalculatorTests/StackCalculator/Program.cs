089274913adebe00d6bcdca1c3361e3a69e27e83
Pavel Averin <st107211@student.spbu.ru>
2023-03-12 21:19:56 +0300

    Completed task "StackCalculatorTests" from Homework 3.

---
+++ Homework3/StackCalculatorTests/StackCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackCalculatorTask;
+
+if (!ListStack.Tests() || !ArrayStack.Tests() || !StackCalculator.Tests())
+{
+    Console.WriteLine("Tests weren't passed");
+            
+}
+else
+{
+    string expression = "1 2 3 * +";
+    var stack = new ListStack();
+    var calculator = new StackCalculator();
+    double result = calculator.Calculate(expression, stack);
+    Console.WriteLine($"Given expression: {expression}");
+    Console.WriteLine($"Result: {result}");
+}
+
+
+
+
