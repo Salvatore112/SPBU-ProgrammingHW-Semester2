3bd3a39509ec68bcfff0b2ded1fe4a2841cd16fa
Pavel Averin <st107211@student.spbu.ru>
2023-03-29 03:58:52 +0300

    update files

---
+++ Homework5/RoutersTask/Edge.cs
@@ -0,0 +1,39 @@
+﻿namespace RoutesTaskSpace;
+
+/// <summary>
+/// Class that represents an edge in a non-oriented graph. 
+/// </summary>
+public class Edge
+{
+    /// <summary>
+    /// Beginning vertex.
+    /// </summary>
+    public int Beginning { get; set; }
+    
+    /// <summary>
+    /// End vertex.
+    /// </summary>
+    public int End { get; set; }
+
+    /// <summary>
+    /// A label that carries certain information
+    /// about an edge (lenght in this case).
+    /// </summary>
+    public int Length { get; set; }
+
+    internal Edge(int beginning, int end, int length)
+    {
+        this.Beginning = beginning;
+        this.End = end;
+        this.Length = length;
+    }
+
+    /// <summary>
+    /// Overriden ToString method used to represent an edge for a task.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{this.End} ({this.Length})";
+    }
+}
