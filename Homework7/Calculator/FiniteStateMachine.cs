b53f5fa952457665d75a2777be9527b155c2b3dc
Pavel Averin <st107211@student.spbu.ru>
2023-04-09 16:37:30 +0300

    Add "FiniteStateMachine.cs" class

---
+++ Homework7/Calculator/FiniteStateMachine.cs
@@ -0,0 +1,32 @@
+﻿namespace CalculatorSpace;
+
+internal static class FiniteStateMachine
+{
+    enum States : int
+    {
+        startState = 0,
+        witingForAnOperandStand = 5,
+        Automotive = 6,
+        Arts = 10,
+        BeautyCare = 11,
+        Fashion = 15
+    }
+
+    private static States currentState = States.startState;
+    static int firstOperand;
+    static int secondOperand;
+    static char Operator;
+
+    public static char Input { get; set; } = '0';
+    public static string Output { get; private set; }
+
+    static void IterateFSM()
+    {
+        switch (currentState)
+        {
+            case (States.startState):
+
+                break;
+        }
+    }
+}
