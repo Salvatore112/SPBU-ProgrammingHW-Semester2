800d28fb7e46817280eceb68b110347f019357ab
Pavel Averin <st107211@student.spbu.ru>
2023-04-09 16:18:18 +0300

    add buttons and change their names

--- Homework7/Calculator/Calculator.Designer.cs
+++ Homework7/Calculator/Calculator.Designer.cs
@@ -28,18 +28,289 @@ partial class Calculator
     /// </summary>
     private void InitializeComponent()
     {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.equalsButton = new System.Windows.Forms.Button();
+            this.floatingPointButton = new System.Windows.Forms.Button();
+            this.eraseButton = new System.Windows.Forms.Button();
+            this.signButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.equalsButton, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.subtractButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.multiplyButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.divideButton, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(296, 75);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(89, 275);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.signButton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.eraseButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 2, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button5, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button6, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button7, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button8, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button9, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button0, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.floatingPointButton, 2, 4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(24, 75);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(250, 275);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(361, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 49);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(86, 113);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 49);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(169, 113);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 49);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 58);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 49);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(86, 58);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(77, 49);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(169, 58);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(77, 49);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(3, 223);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(77, 49);
+            this.button0.TabIndex = 12;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(3, 3);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(83, 49);
+            this.divideButton.TabIndex = 4;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(3, 58);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(83, 49);
+            this.multiplyButton.TabIndex = 5;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(3, 113);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(83, 49);
+            this.subtractButton.TabIndex = 6;
+            this.subtractButton.Text = "-";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(3, 168);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(83, 49);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // equalsButton
+            // 
+            this.equalsButton.Location = new System.Drawing.Point(3, 223);
+            this.equalsButton.Name = "equalsButton";
+            this.equalsButton.Size = new System.Drawing.Size(83, 49);
+            this.equalsButton.TabIndex = 8;
+            this.equalsButton.Text = "=";
+            this.equalsButton.UseVisualStyleBackColor = true;
+            // 
+            // floatingPointButton
+            // 
+            this.floatingPointButton.Location = new System.Drawing.Point(169, 223);
+            this.floatingPointButton.Name = "floatingPointButton";
+            this.floatingPointButton.Size = new System.Drawing.Size(78, 49);
+            this.floatingPointButton.TabIndex = 13;
+            this.floatingPointButton.Text = ",";
+            this.floatingPointButton.UseVisualStyleBackColor = true;
+            // 
+            // eraseButton
+            // 
+            this.eraseButton.Location = new System.Drawing.Point(3, 3);
+            this.eraseButton.Name = "eraseButton";
+            this.eraseButton.Size = new System.Drawing.Size(77, 49);
+            this.eraseButton.TabIndex = 14;
+            this.eraseButton.Text = "C";
+            this.eraseButton.UseVisualStyleBackColor = true;
+            // 
+            // signButton
+            // 
+            this.signButton.Location = new System.Drawing.Point(86, 3);
+            this.signButton.Name = "signButton";
+            this.signButton.Size = new System.Drawing.Size(77, 49);
+            this.signButton.TabIndex = 15;
+            this.signButton.Text = "+/-";
+            this.signButton.UseVisualStyleBackColor = true;
             // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.ClientSize = new System.Drawing.Size(401, 364);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.MinimumSize = new System.Drawing.Size(419, 411);
             this.Name = "Calculator";
             this.Text = "Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
+
+    private TableLayoutPanel tableLayoutPanel1;
+    private Button equalsButton;
+    private Button addButton;
+    private Button subtractButton;
+    private Button multiplyButton;
+    private Button divideButton;
+    private TableLayoutPanel tableLayoutPanel2;
+    private Button signButton;
+    private Button eraseButton;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
+    private Button floatingPointButton;
+    private TextBox textBox1;
 }
