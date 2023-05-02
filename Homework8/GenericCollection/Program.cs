8b2872bfc4bf20bec8eb9f717d64ea871358834a
Pavel Averin <st107211@student.spbu.ru>
2023-05-02 03:22:02 +0300

    Complete task "SkipList" from Homework 8

---
+++ Homework8/GenericCollection/Program.cs
@@ -0,0 +1,13 @@
+﻿using GenericCollectionSpace;
+
+var testList = new SkipList<int>();
+
+testList.Add(1);
+testList.Add(2);
+testList.Add(3);
+
+Console.WriteLine(testList.IndexOf(3));
+
+testList.Clear();
+
+Console.ReadKey();
