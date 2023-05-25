13d1e2d98dfc97a1fdb8b31d8a15645920651ab7
Pavel Averin <st107211@student.spbu.ru>
2023-05-25 20:23:16 +0300

    Delete IEnumerable and add SetUp for tests

--- Homework4/ParseTree.Tests/ParseTreeTests.cs
+++ Homework4/ParseTree.Tests/ParseTreeTests.cs
@@ -2,53 +2,55 @@ namespace ParseTreeSpace.Tests;
 
 public class Tests
 {
-    private static IEnumerable<TestCaseData> Trees
-       => new TestCaseData[]
-       {
-            new TestCaseData(new ParseTree())
-       };
+    ParseTree testTree;
 
-    [TestCaseSource(nameof(Trees))]
-    public void TreeShouldBeAbleToCalculateExampleExpression(IParseTree tree)
+    [SetUp]
+    public void Init()
+    {
+        testTree = new ParseTree();
+    }
+
+    [Test]
+    public void TreeShouldBeAbleToCalculateExampleExpression()
     {
         string expression = "(* (+ 1 1) 2)";
-        Assert.That(tree.CalculateExpression(expression) == 4 &&
-                    tree.PrintTree(expression) == "( * ( + 1 1 ) 2 )");
+        Assert.That(testTree.CalculateExpression(expression) == 4 &&
+                    testTree.PrintTree(expression) == "( * ( + 1 1 ) 2 )");
     }
-    
-    [TestCaseSource(nameof(Trees))]
-    public void MultiplicationByZeroShouldProduceZero(IParseTree tree)
+
+    [Test]
+    public void MultiplicationByZeroShouldProduceZero()
     {
         string expression = "* 9 0";
-        Assert.That(tree.CalculateExpression(expression) < 0.00001 &&
-                    tree.PrintTree(expression) == "( * 9 0 )");
+        Assert.That(testTree.CalculateExpression(expression) < 0.00001 &&
+                    testTree.PrintTree(expression) == "( * 9 0 )");
     }
 
-    [TestCaseSource(nameof(Trees))]
-    public void ExpressionWhereBothOperandsAreTreesShouldBeCalculated(IParseTree tree)
+    [Test]
+    public void ExpressionWhereBothOperandsAreTreesShouldBeCalculated()
     {
         string expression = "* (+ 3 1) (/ 14 2)";
-        Assert.That(tree.CalculateExpression(expression), Is.EqualTo(28).Within(0.00001));
+        Assert.That(testTree.CalculateExpression(expression), Is.EqualTo(28).Within(0.00001));
     }
 
-    [TestCaseSource(nameof(Trees))]
-    public void InvalidCharactersInTheExpressionShouldCauseAnException(IParseTree tree)
+    [Test]
+    public void InvalidCharactersInTheExpressionShouldCauseAnException()
     {
         string expression = "+ a b";
-        Assert.Throws<InvalidExpressionException>(() => tree.CalculateExpression(expression));
+        Assert.Throws<InvalidExpressionException>(() => testTree.CalculateExpression(expression));
     }
 
-    [TestCaseSource(nameof(Trees))]
-    public void ExpressionStartingWithAnOperandShouldBeConsideredInvalid(IParseTree tree)
+    [Test]
+    public void ExpressionStartingWithAnOperandShouldBeConsideredInvalid()
     {
         string expression = "5 + 5";
-        Assert.Throws<InvalidExpressionException>(() => tree.CalculateExpression(expression));
+        Assert.Throws<InvalidExpressionException>(() => testTree.CalculateExpression(expression));
     }
 
-    [TestCaseSource(nameof(Trees))]
-    public void ExpressionsWithMissingOperandsShouldCauseAnException(IParseTree tree)
+    [Test]
+    public void ExpressionsWithMissingOperandsShouldCauseAnException()
     {
         string expression = "* 3";
-        Assert.Throws<InvalidExpressionException>(() => tree.CalculateExpression(expression));
+        Assert.Throws<InvalidExpressionException>(() => testTree.CalculateExpression(expression));
     }
 }
