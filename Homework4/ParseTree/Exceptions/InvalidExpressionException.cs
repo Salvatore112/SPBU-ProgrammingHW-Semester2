43acde2a9d4d10399318de1e2abdb8cf4142674c
Pavel Averin <st107211@student.spbu.ru>
2023-05-25 20:30:07 +0300

    Add a comment for the exception

--- Homework4/ParseTree/Exceptions/InvalidExpressionException.cs
+++ Homework4/ParseTree/Exceptions/InvalidExpressionException.cs
@@ -1,10 +1,12 @@
 ﻿namespace ParseTreeSpace;
 
+/// <summary>
+/// Exception that is thrown when you try and enter invalid characters.
+/// </summary>
 internal class InvalidExpressionException : Exception
 {
     internal InvalidExpressionException(string message) : base(message)
     {
 
     }
-
 }
