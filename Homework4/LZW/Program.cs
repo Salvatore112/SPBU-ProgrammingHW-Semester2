bcb80ab9d1addb540092b9564c3318fe2e690b44
Pavel Averin <pasha2281337alfa@gmail.com>
2023-09-06 17:35:49 +0300

    remove extra strings

--- Homework4/LZW/Program.cs
+++ Homework4/LZW/Program.cs
@@ -1,17 +1,12 @@
 ﻿using LZWTask;
 
 var LZW = new LZW();
+
 string path1 = @"D:\LZWTask\files\test1.txt";
 string path2 = @"D:\LZWTask\files\exeTest.exe";
 string path2_2 = @"D:\LZWTask\exeTest.zipped";
 string path2_3 = @"D:\LZWTask\exeTestO.exe";
 string path3 = @"D:\LZWTask\files\test.txt";
 string path4 = @"D:\LZWTask\files\test.jpg";
-LZW.Compress(path2);
-
-string w = "AAAFFFA";
 
 
-var l = LZW.Compresss(w);
-
-Console.WriteLine(LZW.Decompress(l));
